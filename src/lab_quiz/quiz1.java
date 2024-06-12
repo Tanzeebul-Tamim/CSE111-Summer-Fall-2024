d2327ba0148aa8051a312c584f1d9a9fa41efff0
Tanzeebul Tamim <tamim200091@gmail.com>
2024-06-12 11:48:02 +0600

    theory quiz1 added

--- src/lab_quiz/quiz1.java
+++ src/lab_quiz/quiz1.java
@@ -2,18 +2,30 @@ package lab_quiz;
 
 import java.util.Scanner;
 
-public class quiz1 {
+/*
+    Write a Java program which takes the deposit amount and time period (in years) as user input, and calculates the interest and total receivable amount using the formula (deposit * rate * time) / 100. Calculate the interest and total amount.
+
+    Sample Input:
+    75000
+    5
+
+    Sample Output:
+    Interest: 37500.00
+    The total receivable amount after 5 years is: 112500.00
+ */
+
+public class Quiz1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a deposit amount:");
         float deposit = sc.nextFloat();
 
-        System.out.println("Enter time period (in years):");
+        System.out.println("\nEnter time period (in years):");
         int time = sc.nextInt();
 
         int rate;
-        if (deposit < 10000)  {
+        if (deposit < 10000) {
             rate = 0;
         } else if (deposit >= 1000 && deposit < 50000) {
             rate = 8;
@@ -27,7 +39,7 @@ public class quiz1 {
 
         float interest = (deposit * rate * time) / 100;
 
-        System.out.format("Interest: %.2f", interest);
+        System.out.format("\nInterest: %.2f", interest);
         System.out.format("\nThe total receivable amount after %d years is: %.2f", time, interest + deposit);
     }
 }
---
+++ src/theory_quiz/Quiz1.java
@@ -0,0 +1,55 @@
+package theory_quiz;
+
+import java.util.Scanner;
+
+/*
+    Write a Java program that takes an integer n as user input and calculates a sum based on whether the numbers from 1 to n are odd or even. 
+    For even numbers, the program adds the cube of the number to the sum. 
+    For odd numbers, it adds the square of the number to the sum.
+    The program should then print the calculated sum for every number from n down to 1.
+
+    Example series (n = 3):
+    For i = 1: 1^2 = 1
+    For i = 2: 1^2 + 2^3 = 1 + 8 = 9
+    For i = 3: 1^2 + 2^3 + 3^2 = 1 + 8 + 9 = 18
+
+    Sample Input:
+    3
+
+    Sample Output:
+    18
+    9
+    1
+ */
+
+class Calculate {
+    public int formula(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += (int) Math.pow(i, 3);
+            } else {
+                sum += (int) Math.pow(i, 2);
+            }
+        }
+        return sum;
+    }
+}
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
+        System.out.println();
+        sc.close();
+
+        Calculate calc = new Calculate();
+
+        for (int i = n; i >= 1; i--) {
+            int result = calc.formula(i);
+            System.out.println(result);
+        }
+    }
+}
