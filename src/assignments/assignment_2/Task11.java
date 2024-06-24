66c9f10f94e558568a82527f7106fdb3c8949135
Tanzeebul Tamim <tamim200091@gmail.com>
2024-06-24 23:52:20 +0600

    assignment 2 completed

--- src/assignments/assignment_2/Task11.java
+++ src/assignments/assignment_2/Task11.java
@@ -1,10 +1,18 @@
 package assignments.assignment_2;
 
+import java.util.Scanner;
+
 public class Task11 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter an integer:");
+        int userInput = sc.nextInt();
+        sc.close();
+
         // TASK A
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < userInput; i++) {
+            for (int j = 0; j < userInput; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -12,8 +20,8 @@ public class Task11 {
 
         System.out.println();
 
-        // TASK B  
-        for (int i = 0; i < 5; i++) {
+        // TASK B
+        for (int i = 0; i < userInput; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
--- src/assignments/assignment_2/Task8.java
+++ src/assignments/assignment_2/Task8.java
@@ -20,26 +20,24 @@ public class Task8 {
     static void countPrimes(int num1, int num2) {
         int count = 0;
 
-        if (num1 < num2) {
-            for (int i = num1; i <= num2; i++) {
-                if (isPrime(i)) {
-                    count++;
-                }
-            }
-        } else {
-            for (int i = num2; i <= num1; i++) {
-                if (isPrime(i)) {
-                    count++;
-                }
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        for (int i = num1; i <= num2; i++) {
+            if (isPrime(i)) {
+                count++;
             }
         }
 
         if (count == 0) {
-            System.out.format("\nThere're no prime numbers between %d and %d", num1, num2);
+            System.out.format("\nThere aren't any prime numbers between %d and %d.", num1, num2);
         } else if (count == 1) {
-            System.out.format("\nThere is only one prime number between %d and %d", count, num1, num2);
+            System.out.format("\nThere is only one prime number between %d and %d.", count, num1, num2);
         } else {
-            System.out.format("\nThere are %d prime numbers between %d and %d", count, num1, num2);
+            System.out.format("\nThere are %d prime numbers between %d and %d.", count, num1, num2);
         }
     }
 
