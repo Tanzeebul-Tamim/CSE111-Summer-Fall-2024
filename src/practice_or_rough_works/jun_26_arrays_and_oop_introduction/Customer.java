f0f551f9f6ed1c1c26786c247ac83d0dd95a3367
Tanzeebul Tamim <tamim200091@gmail.com>
2024-06-26 16:28:20 +0600

    26 june class materials added

---
+++ src/practice_or_rough_works/jun_26_arrays_and_oop_introduction/Customer.java
@@ -0,0 +1,10 @@
+package practice_or_rough_works.jun_26_arrays_and_oop_introduction;
+
+public class Customer {
+    String name;
+    int age;
+
+    public void show() {
+        System.out.println("Customer Info: " + this.name + " (" + this.age + ")");
+    }
+}
