3d201bddf46fe78919dd4d5730596cf4fb640fa7
Tanzeebul Tamim <tamim200091@gmail.com>
2024-06-30 02:40:06 +0600

    assignment 3 task 4 completed

--- src/assignments/assignments_3/string/Task4.java
+++ src/assignments/assignments_3/string/Task4.java
@@ -1,28 +1,41 @@
 package assignments.assignments_3.string;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean dupFound = false;
+        boolean flag = false;
+        ArrayList<String> inputWords = new ArrayList<>();
 
         System.out.println("Enter a word in small letters:");
 
-        do {
-            dupFound = false;
+        checkDuplicate: do {
+            flag = false;
             String userInput = sc.next();
-
-            int ascii = 0;
-            for (int i = 0; i < userInput.length(); i++) {
-                char currentLetter = userInput.charAt(i);
-                if (i != 0 && ascii == (int) currentLetter) {
-                    System.out.println("prev " + ascii);
-                    System.out.println("curr " + (int) currentLetter);
-                    continue;
+            
+            for (String inputWord : inputWords) {
+                if (inputWord.equalsIgnoreCase(userInput)) {
+                    System.out.printf("%s has already been input.\nPlease enter another word.\n", userInput);
+                    flag = true;
+                    continue checkDuplicate;
                 }
+            }
 
-                ascii = (int) currentLetter;
+            inputWords.add(userInput);
+
+            char[] dupLetters = new char[userInput.length()];
+            checkDupLetters: for (int i = 0; i < userInput.length(); i++) {
+                char currentLetter = userInput.charAt(i);
+
+                if (i != 0) {
+                    for (char dupLetter : dupLetters) {
+                        if (dupLetter == currentLetter) {
+                            continue checkDupLetters;
+                        }
+                    }
+                }
 
                 int count = 1;
                 for (int j = i + 1; j < userInput.length(); j++) {
@@ -33,20 +46,21 @@ public class Task4 {
                 }
 
                 if (count > 1) {
-                    dupFound = true;
+                    dupLetters[i] = currentLetter;
+                    flag = true;
                     System.out.printf("\"%s\" has been counted %d times in the word \"%s\".\n", currentLetter,
                             count,
                             userInput);
                 }
             }
 
-            if (dupFound) {
+            if (flag) {
                 System.out.println("Please enter another word.");
             } else {
-                System.out.printf("You entered %s", userInput);
+                System.out.printf("You entered %s.", userInput);
                 sc.close();
                 break;
             }
-        } while (dupFound);
+        } while (flag);
     }
 }
