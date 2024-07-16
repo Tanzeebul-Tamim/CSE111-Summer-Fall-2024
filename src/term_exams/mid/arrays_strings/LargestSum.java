b9089c7d96f360de66d49372b17e2567a0ee4aac
Tanzeebul Tamim <tamim200091@gmail.com>
2024-07-16 13:22:16 +0600

    midterm tasks added

---
+++ src/term_exams/mid/arrays_strings/LargestSum.java
@@ -0,0 +1,72 @@
+package term_exams.mid.arrays_strings;
+
+/*
+    You are given an array of strings each containing a mix of letters, special characters, and digits. You need to write a Java program that calculates the sum of the digits contained in each string and output the largest sum along with the string that it came from.
+
+    Sample Input:
+    String[] input = {
+        "a1b2@c3",
+        "D4#e5F6",
+        "g!H7i8%",
+        "jK9$l0M",
+        "N@1o2p3"
+    };
+
+    Sample Output:
+    The maximum sum is 15 which came from the string D4#e5F6.
+
+    Explanation:
+    - For "a1b2@c3", the sum of digits is 1 + 2 + 3 = 6.
+    - For "D4#e5F6", the sum of digits is 4 + 5 + 6 = 15.
+    - For "g!H7i8%", the sum of digits is 7 + 8 = 15.
+    - For "jK9$l0M", the sum of digits is 9 + 0 = 9.
+    - For "N@1o2p3", the sum of digits is 1 + 2 + 3 = 6.
+    - The string with the largest sum is "D4#e5F6" with a sum of 15.
+*/
+
+public class LargestSum {
+    static int calcSum(String code) {
+        int result = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            char letter = code.charAt(i);
+            int ascii = (int) letter;
+
+            if (ascii >= 48 && ascii <= 57) {
+                int digit = ascii - 48;
+                result += digit;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] inputs = {
+                "a1b2@c3",
+                "D4#e5F6",
+                "g!H7i8%",
+                "jK9$l0M",
+                "N@1o2p3",
+                "Q#r4S5t",
+                "u!V6w7X",
+                "yZ8@9a0",
+                "!b1C2#d",
+                "E3$f4G5"
+        };
+
+        String maxStr = "";
+        int maxSum = 0;
+
+        for (String input : inputs) {
+            int sum = calcSum(input);
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxStr = input;
+            }
+        }
+
+        System.out.printf("The maximum sum is %d which came from the string %s.\n", maxSum, maxStr);
+    }
+}
---
+++ src/term_exams/mid/oop/ExamTester.java
@@ -0,0 +1,67 @@
+package term_exams.mid.oop;
+
+/*
+    You are building a system to simulate the process of creating and managing an examination.
+        ● When the constructor is invoked, it sets the name, time, and the number of topics for the exam.
+        ● The printInfo() method prints the examination details.
+        ● The setTopic() method is used for setting the topics for the examination.
+        ● The endExam() method is used to end a examination.
+
+    Expected output:
+
+    1 ==========
+    2 ==========
+    Midterm Examination:
+    Time left: 60.0 mins
+    Topics chosen:
+    Math
+    Physics
+    3 ==========
+    4 ==========
+    Couldn't add topic Biology.
+    5 ==========
+    Midterm Examination:
+    Time left: 60.0 mins
+    Topics chosen:
+    Math
+    Physics
+    Chemistry
+    6 ==========
+    The Midterm examination has ended.
+    7 ==========
+    8 ==========
+    Finals Examination:
+    Time left: 120.0 mins
+    No topics were chosen.
+    9 ==========
+    The Finals examination has ended.
+    10 ==========
+*/
+
+public class ExamTester {
+    public static void main(String[] args) {
+        Exam exam1 = new Exam();
+        Exam exam2 = new Exam();
+        exam1.createExam("Midterm", 60.0, 3);
+        exam2.createExam("Finals", 120.0, 4);
+        System.out.println("1 ==========");
+        exam1.setTopic("Math");
+        exam1.setTopic("Physics");
+        System.out.println("2 ==========");
+        exam1.printInfo();
+        System.out.println("3 ==========");
+        exam1.setTopic("Chemistry");
+        System.out.println("4 ==========");
+        exam1.setTopic("Biology");
+        System.out.println("5 ==========");
+        exam1.printInfo();
+        System.out.println("6 ==========");
+        System.out.println(exam1.endExam());
+        System.out.println("7 ==========");
+        System.out.println("8 ==========");
+        exam2.printInfo();
+        System.out.println("9 ==========");
+        System.out.println(exam2.endExam());
+        System.out.println("10 ==========");
+    }
+}
