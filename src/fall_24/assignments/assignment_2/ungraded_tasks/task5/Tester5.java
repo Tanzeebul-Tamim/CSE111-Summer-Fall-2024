95a5b7e5e5b82fae4e746d6e2aabaa0d567d6e95
Tanzeebul Tamim <tamim200091@gmail.com>
2024-11-16 15:30:17 +0600

    fall 24 assignment 2 ungraded tracing tasks completed

---
+++ src/fall_24/assignments/assignment_2/ungraded_tasks/task5/Tester5.java
@@ -0,0 +1,16 @@
+package fall_24.assignments.assignment_2.ungraded_tasks.task5;
+
+public class Tester5 {
+    public static void main(String args[]) {
+        Test1 t1 = new Test1();
+        t1.methodB(5, -8);
+        Test1 t2 = new Test1();
+        t2.methodA();
+    }
+}
+// Outputs:
+/*
+    11 5 16
+    22 7 29
+    17 3 23
+ */
