ec00e960b3c3d1333f11ea6829c7a10635e1d140
Tanzeebul Tamim <tamim200091@gmail.com>
2024-11-30 05:55:25 +0600

    Fall 2024 Assignment 3 tracing completed

--- src/fall_24/assignments/assignment_3/ungraded_tasks/task2/Tester2.java
+++ src/fall_24/assignments/assignment_3/ungraded_tasks/task2/Tester2.java
@@ -2,8 +2,9 @@ package fall_24.assignments.assignment_3.ungraded_tasks.task2;
 
 public class Tester2 {
     public static void main(String[] args) {
-        Test2 t = new Test2();
-        t.methodA(3, 4);
+        // Test2 t = new Test2();
+        // t.methodA(3, 4);
+        System.out.println(21+3%4*2.0);
     }
 }
 
